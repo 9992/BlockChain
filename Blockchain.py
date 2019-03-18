74a06602a1b325b9450de863fcf0975aab5c67bd
또르 <hansol416@naver.com>
2019-03-18 11:31:44 +0900

    Connect Flask & Blockchain

--- Blockchain.py
+++ Blockchain.py
@@ -1,13 +1,19 @@
+#-*- coding:utf-8 -*-
+
 import hashlib
 import json
+from textwrap import dedent
 from time import time
+from uuid import uuid4
+from flask import Flask, jsonify, request 
+
 
 class Blockchain(object):
     def __init__(self):
         self.current_transactions = []
         self.chain = []
 
-        # 제네시스 블록 생성
+        """ 제네시스 블록 생성 """
         self.new_block(previous_hash=1, proof=100)
 
 
@@ -90,6 +96,79 @@ class Blockchain(object):
             input ( 둘다 <int> )
             return <bool>
         """
-        guess = f'{last_proof}{proof}'.encode()
+
+        guess = '{last_proof}{proof}'.format(last_proof,proof).encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
         return guess_hash[:4] == "0000"
+
+
+# Instantiate our Node
+app = Flask(__name__)
+
+# Generate a globally unique address for this node
+node_identifier = str(uuid4()).replace('-', '')
+
+# Instantiate the Blockchain
+blockchain = Blockchain()
+    
+@app.route('/transactions/new', methods =['POST'])
+def new_transaction():
+    values = request.get_json()
+    
+    # 요청된 필드가 POST 된 데이터인지 확인하는 
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+    
+    # 새로운 거래 생성
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+    
+    response = {'message' : 'Transaction will be added to Block {index}'.format(index)}
+    return jsonify(response), 201
+
+@app.route('/mine', methods = ['GET'])
+def mine():
+    #다음 블록의 proof 값을 얻어내기 위해 POW 알고리즘을 수행한다. 
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+    
+    #proof 값을 찾으면(채굴에 성공하면) 보상을 준다.
+    #sender의 주소를 0으로 한다. (원래 거래는 송신자, 수신자가 있어야 하는데 챌굴에 대한 보상으로 얻은 코인은 sender 가 없다.)
+    blockchain.new_transaction(
+        sender="0",
+        recipient=node_identifier,
+        amount=1,
+    )
+    
+    #
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof, previous_hash)
+    
+    response = {
+        'message' : "New Block Forged",
+        'index' : block['index'],
+        'transactions' : block['transactions'],
+        'proof' : block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+    return jsonify(response), 200
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+    'chain' : blockchain.chain,
+    'length' : len(blockchain.chain),
+    }
+
+    return jsonify(response),200
+
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--port', default=5000, type=int, help='port to listen on')
+    args = parser.parse_args()
+    port = args.port
+    app.run(host='0.0.0.0', port= 5000)
