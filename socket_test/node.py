0168b6cefc9db0fdc21cb3efebcbead2b81e6d60
또르 <hansol416@naver.com>
2019-04-03 00:12:36 +0900

    Node.py Error..

--- socket_test/node.py
+++ socket_test/node.py
@@ -5,6 +5,7 @@ UDP_PORT = 5000
 
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 sock.bind((UDP_IP,UDP_PORT))
+count = 0
 
 while True:
     data, addr = sock.recvfrom(1024)
--- socket_test/server.py
+++ socket_test/server.py
@@ -2,7 +2,8 @@ import socket
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5001
-MESSAGE = "Hello, UDP"
+MESSAGE = "Hello, UDP".encode()
 
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 sock.sendto(MESSAGE, (UDP_IP,UDP_PORT))
+print(MESSAGE)
