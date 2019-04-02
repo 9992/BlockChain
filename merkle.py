c69bc03cd51ad47b7570dd5d24a7b33e98e438a2
또르 <hansol416@naver.com>
2019-04-02 17:09:24 +0900

    MekleHash Update

--- merkle.py
+++ merkle.py
@@ -1,30 +1,28 @@
-import json
 import hashlib
+from codecs import encode, decode
 
-def merkle(hashList):
-    if len(hashList)==1:
-        return hashList[0]
-    newHashList = []
+def merkle(hList):
+    if len(hList) == 1:
+        return hList[0]
+    newHList = []
+    for i in range(0, len(hList)-1, 2):
+        newHList.append(merklehhash(hList[i],hList[i+1]))
+    if len(hList) % 2 == 1: 
+        newHList.append(merklehhash(hList[-1],hList[-1]))
+    return merkle(newHList)
 
-    for i in  range(0,len(hashList)-1,2):
-        newHashList.append(calc_hash(hashList[i],hashList[i+1]))
-    if len(hashList) % 2 == 1:
-        newHashList.append(calc_hash(hashList[-1],hashList[-1]))
-    return merkle(newHashList)
-
-# 해시의 기본 원리는 모든 문자스트림을 리틀 엔디언 순으로 읽어들여 나눈다.
-# 따라서 해시와 해시를 더할 때에는 역순으로 더 할 수 있도록 해야한다.
-def calc_hash(a_list,b_list):
-    a = a_list[::-1].hexdigest()
-    b = b_list[::-1].hexdigest()
-    result_hash = hashlib.sha256(hashlib.sha256(a+b).hexdigest()).hexdigest()
-    return result_hash[::-1]
+def merklehhash(a, b):
+    a1 = decode(a[::-1],'hex')    
+    b1 = decode(b[::-1],'hex')
+    result = hashlib.sha256(a1+b1).digest()
+    print("result : ", result)
+    return encode(result,'hex')[::-1]
 
 txHashes = [
 "8c14f0db3df150123e6f3dbbf30f8b955a8249b62ac1d1ff16284aefa3d06d87",
 "fff2525b8931402dd09222c50775608f75787bd2b87e56995a7bdd30f79702c4",
 "6359f0868171b1d194cbee1af2f16ea598ae8fad666d9b012c8ed2b79a236ec4",
-"e9a66845e05d5abc0ad04ec80f774a7e585c6e8db975962d069a522137b80c1d",
+"e9a66845e05d5abc0ad04ec80f774a7e585c6e8db975962d069a522137b80c1d"
 ]
 
 print(merkle(txHashes))
